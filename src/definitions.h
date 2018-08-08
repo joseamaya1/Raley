0c11eea5c430b87294f38603ffa2562590d3bd21
joseamaya1 <joseamaya12331@gmail.com>
2018-08-08 15:45:27 -0700

    Changes applicable to source code

--- src/definitions.h
+++ src/definitions.h
@@ -20,15 +20,15 @@
 #ifndef FS_DEFINITIONS_H_877452FEC245450C9F96B8FD268D8963
 #define FS_DEFINITIONS_H_877452FEC245450C9F96B8FD268D8963
 
-static constexpr auto STATUS_SERVER_NAME = "OTX Server";
-static constexpr auto STATUS_SERVER_VERSION = "3";
+static constexpr auto STATUS_SERVER_NAME = "RaleySoft Server";
+static constexpr auto STATUS_SERVER_VERSION = "1";
 static constexpr auto MINOR_VERSION = "10";
-static constexpr auto STATUS_SERVER_DEVELOPERS = "Matt Gomez, Leandro Brewster, Erick Nunes, Joao Leonardo Pereira, Bruno Carvalho, Charles Kassab, OTX Contributors and The Forgotten Server Developers";
-static constexpr auto GIT_REPO = "https://gitlab.com/malucooo/Otxserver-Global/";
+static constexpr auto STATUS_SERVER_DEVELOPERS = "RaleySoft, OTX Contributors and The Forgotten Server Developers";
+static constexpr auto GIT_REPO = "https://github.com/joseamaya1/Raley/";
 
 static constexpr auto CLIENT_VERSION_MIN = 1100;
-static constexpr auto CLIENT_VERSION_MAX = 1171;
-static constexpr auto CLIENT_VERSION_STR = "11.71";
+static constexpr auto CLIENT_VERSION_MAX = 1180;
+static constexpr auto CLIENT_VERSION_STR = "11.80";
 
 static constexpr auto AUTHENTICATOR_DIGITS = 6U;
 static constexpr auto AUTHENTICATOR_PERIOD = 30U;
